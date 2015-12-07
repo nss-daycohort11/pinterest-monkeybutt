69e3e4c239f611a051e3c7dd80a7f5de3ce32215
Caleb Scholze <caleb.scholze@gmail.com>
2015-12-07 15:44:39 -0600

    new poops adding to firebase

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -2,23 +2,27 @@ angular
 	.module("MonkeyButt")
 	.controller('DashboardController', DashboardController);
 
-function DashboardController () {
+function DashboardController ($firebaseArray) {
 	var vm = this;
+	vm.ref = new Firebase("https://monkeybutt.firebaseio.com/poops");
+	vm.uid = vm.ref.getAuth().uid;
+	console.log("uid",vm.uid);
 
 	vm.greeting = 'Hola Terra!';
 	console.log(vm.greeting);
 
 	vm.newPoop = {
-		createdBy: "",
-		poopedBy: [],
-		imageURL: "",
+		createdBy: vm.uid,
+		poopedBy: [vm.uid],
+		poopyPic: "",
 		contentURL: "",
-	}
-
-
+	};
 
 	vm.addPoop = function() {
 		console.log("smearing poop ALL OVER YOUR WALL!!");
-		
-	}
+		// add poop to firebase array
+		vm.poops = $firebaseArray(vm.ref);
+		vm.poops.$add(vm.newPoop);
+		console.log("vm.poops",vm.poops);
+	};
 }
