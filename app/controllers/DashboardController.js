fa26b575dd8811d1592e45606ae6cdcdf898a10c
Mike Ulvila <mikeulvila@me.com>
2015-12-08 13:08:46 -0600

    pin and trash button added

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -10,7 +10,7 @@ function DashboardController ($firebaseArray, $firebaseAuth) {
 	vm.poops = $firebaseArray(vm.poopsRef);
 	console.log("poops",vm.poops);
 	vm.uid = vm.ref.getAuth().uid;
-
+	vm.likesRef = new Firebase("https://monkeybutt.firebaseio.com/likes");
 	vm.newPoop = {
 		createdBy: vm.uid,
 		poopedBy: [vm.uid],
@@ -27,5 +27,11 @@ function DashboardController ($firebaseArray, $firebaseAuth) {
 		// add poop to firebase array
 		vm.poops.$add(vm.newPoop);
 		console.log("vm.poops",vm.poops);
+	};
+	// pin poop to users board
+	vm.pinPoop = function(poop) {
+		vm.likesRef.push({poopID: poop.$id, userID: vm.uid});
+		console.log("poop:", poop.$id);
+		console.log("you just pinned that poop");
 	};
 }
