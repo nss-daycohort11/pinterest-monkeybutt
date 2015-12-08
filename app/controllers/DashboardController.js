5e16c673460ee836a7274b351d0bf34a87bce3fb
Caleb Scholze <caleb.scholze@gmail.com>
2015-12-08 13:08:53 -0600

    rebased development

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -6,10 +6,9 @@ function DashboardController ($firebaseArray, $firebaseAuth) {
 	var vm = this;
 	vm.ref = new Firebase("https://monkeybutt.firebaseio.com");
  	vm.authData = $firebaseAuth(vm.ref);
-	vm.poopsRef = new Firebase("https://monkeybutt.firebaseio.com/poops");
-	vm.poops = $firebaseArray(vm.poopsRef);
-	console.log("poops",vm.poops);
 	vm.uid = vm.ref.getAuth().uid;
+	vm.poopsRef = new Firebase("https://monkeybutt.firebaseio.com/poops");
+	vm.poopsArray = $firebaseArray(vm.poopsRef);
 
 	vm.searchText = "";
 	console.log("searchText", vm.searchText);
@@ -21,27 +20,32 @@ function DashboardController ($firebaseArray, $firebaseAuth) {
 
 	vm.newPoop = {
 		createdBy: vm.uid,
-		poopedBy: [vm.uid],
 		poopyPic: "",
 		contentURL: "",
 		keywords: "",
 	};
 
- 	vm.logOut = function () {	
- 		console.log("I'm LOGGING OUT!!!");
- 		vm.authData.$unauth();
- 	};
-
 	vm.addPoop = function() {
 		console.log("smearing poop ALL OVER YOUR WALL!!");
-		// add poop to firebase array
-		vm.poops.$add(vm.newPoop);
-		console.log("vm.poops",vm.poops);
-		
+		var poopRef = vm.poopsRef.push(vm.newPoop);
+
 		// reset modal form inputs
 		vm.newPoop.poopyPic = "";
 		vm.newPoop.contentURL = "";
 		vm.newPoop.keywords = "";
 	};
 
-};
+	vm.getUserMadePoops = function() {
+		var userMadePoopsRef = vm.ref.child("poops");
+    var query = userMadePoopsRef.orderByChild("createdBy").equalTo(vm.uid);
+    vm.poopsArray = $firebaseArray(query);
+	};
+
+	vm.getUserPoops = function() {
+	};
+
+ 	vm.logOut = function () {	
+ 		console.log("I'm LOGGING OUT!!!");
+ 		vm.authData.$unauth();
+ 	};
+}
